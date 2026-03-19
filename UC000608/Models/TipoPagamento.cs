2554c0fc0aedf823b71ddfecf0d6ff0c9577afaa
Vivian <vivianhelenaeloi@gmail.com>
2026-03-19 23:19:01 +0000

    Criação do ficheiro Tipo de Pagamento teste

---
+++ UC000608/Models/TipoPagamento.cs
@@ -0,0 +1,10 @@
+﻿namespace UC000608.Models
+{
+    public class TipoPagamento
+    {
+        public int IdTipoPagamento { get; set; }
+        public string Descricao { get; set; }
+        public double PorcentagemDesconto { get; set; }
+
+    }
+}
