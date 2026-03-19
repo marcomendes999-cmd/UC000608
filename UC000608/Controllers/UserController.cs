dc45dfd6c2882de733540dda44a46f2a615b4346
marli29 <marco.mendes@marli.pt>
2026-03-19 19:33:57 +0000

    Class user
    Class user

---
+++ UC000608/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UC000608.Models;
+
+namespace UC000608.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<User> listausers = new List<User>();
+
+            User user1 = new User
+            { 
+                Id = 1,
+                Name = "António Miranda"
+            };
+
+            listausers.Add(user1);
+
+            return View(listausers);
+        }
+    }
+}
