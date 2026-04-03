be545566980d0057c4531f0190534e02c22f6886
MuscledDog <lemos.vasco@gmail.com>
2026-04-03 14:51:43 +0100

    Plano Treino com index da view/home alterado

---
+++ UC000608/Controllers/PlanoTreinoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UC000608.Models;
+
+namespace UC000608.Controllers
+{
+    public class PlanoTreinoController : Controller
+    {
+        private static List<PlanoTreino> planos = new List<PlanoTreino>
+{
+    new PlanoTreino
+    {
+        Id = 1,
+        Nome = "Plano Raiz",
+        Horario = "2x por semana",
+        Capacidade = 12,
+        Treinador = "Treinador A",
+        TipoDeAula = "Fundamentos"
+    },
+    new PlanoTreino
+    {
+        Id = 2,
+        Nome = "Plano Impulso",
+        Horario = "3x por semana",
+        Capacidade = 10,
+        Treinador = "Treinador B",
+        TipoDeAula = "Treino Funcional"
+    },
+    new PlanoTreino
+    {
+        Id = 3,
+        Nome = "Plano Fusão",
+        Horario = "4x por semana",
+        Capacidade = 8,
+        Treinador = "Treinador C",
+        TipoDeAula = "Cross Training"
+    },
+    new PlanoTreino
+    {
+        Id = 4,
+        Nome = "Plano Titan",
+        Horario = "5x por semana",
+        Capacidade = 6,
+        Treinador = "Treinador D",
+        TipoDeAula = "Alta Intensidade"
+    },
+    new PlanoTreino
+    {
+        Id = 5,
+        Nome = "Plano Supernova",
+        Horario = "6x por semana",
+        Capacidade = 4,
+        Treinador = "Treinador Elite",
+        TipoDeAula = "Performance Máxima"
+    }
+};
+
+        public IActionResult Index()
+        {
+            return View(planos);
+        }
+    }
+}
---
+++ UC000608/Models/PlanoTreino.cs
@@ -0,0 +1,12 @@
+﻿namespace UC000608.Models
+{
+    public class PlanoTreino
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Horario { get; set; }
+        public int Capacidade { get; set; }
+        public string? Treinador { get; set; }
+        public string? TipoDeAula { get; set; }
+    }
+}
