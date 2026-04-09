c2d944d792b29fd4c63055080a045f5779571651
Paula <paularoessle@gmail.com>
2026-04-09 22:20:10 +0100

    Commit Especialidade

---
+++ UC000608/Controllers/EspecialidadeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using UC000608.Models;
+
+namespace UC000608.Controllers
+{
+    public class EspecialidadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Especialidade> lista=new List<Especialidade>();
+            Especialidade Esp1 = new Especialidade();
+            Esp1.Nome = "Crossfit";
+
+            Especialidade Esp2 = new Especialidade();
+            Esp2.Nome = "Yoga";
+
+            Especialidade Esp3 = new Especialidade();
+            Esp3.Nome = "Ciclying";
+
+            lista.Add(Esp1);
+            lista.Add(Esp2);
+            lista.Add (Esp3);
+
+            return View(lista);
+        }
+    }
+}
---
+++ UC000608/Models/Especialidade.cs
@@ -0,0 +1,8 @@
+﻿namespace UC000608.Models
+{
+    public class Especialidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
