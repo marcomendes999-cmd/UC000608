5095e17cf3f75bdcb4af65e3dcfb5b5f356f3f75
MuscledDog <lemos.vasco@gmail.com>
2026-04-16 20:09:55 +0100

    PlanoTreino

--- UC000608/Controllers/PlanoTreinoController.cs
+++ UC000608/Controllers/PlanoTreinoController.cs
@@ -6,56 +6,54 @@ namespace UC000608.Controllers
 {
     public class PlanoTreinoController : Controller
     {
-     
-        // Lista estática de Planos
         private static List<PlanoTreino> planos = new List<PlanoTreino>
-        {
-            new PlanoTreino
-            {
-                Id = 1,
-                Nome = "Plano Raiz",
-                Horario = "2x por semana",
-                Capacidade = 12,
-                Treinador = "Carlos Treinador",
-                TipoDeAula = "Fundamentos"
-            },
-            new PlanoTreino
-            {
-                Id = 2,
-                Nome = "Plano Impulso",
-                Horario = "3x por semana",
-                Capacidade = 10,
-                Treinador = "Helena Coach",
-                TipoDeAula = "Treino Funcional"
-            },
-            new PlanoTreino
-            {
-                Id = 3,
-                Nome = "Plano Fusão",
-                Horario = "4x por semana",
-                Capacidade = 8,
-                Treinador = "Rui Instrutor",
-                TipoDeAula = "Cross Training"
-            },
-            new PlanoTreino
-            {
-                Id = 4,
-                Nome = "Plano Titan",
-                Horario = "5x por semana",
-                Capacidade = 6,
-                Treinador = "Carlos Treinador",
-                TipoDeAula = "Alta Intensidade"
-            },
-            new PlanoTreino
-            {
-                Id = 5,
-                Nome = "Plano Supernova",
-                Horario = "6x por semana",
-                Capacidade = 4,
-                Treinador = "Helena Coach",
-                TipoDeAula = "Performance Máxima"
-            }
-        };
+{
+    new PlanoTreino
+    {
+        Id = 1,
+        Nome = "Plano Raiz",
+        Horario = "2x por semana",
+        Capacidade = 12,
+        Treinador = "Treinador A",
+        TipoDeAula = "Fundamentos"
+    },
+    new PlanoTreino
+    {
+        Id = 2,
+        Nome = "Plano Impulso",
+        Horario = "3x por semana",
+        Capacidade = 10,
+        Treinador = "Treinador B",
+        TipoDeAula = "Treino Funcional"
+    },
+    new PlanoTreino
+    {
+        Id = 3,
+        Nome = "Plano Fusão",
+        Horario = "4x por semana",
+        Capacidade = 8,
+        Treinador = "Treinador C",
+        TipoDeAula = "Cross Training"
+    },
+    new PlanoTreino
+    {
+        Id = 4,
+        Nome = "Plano Titan",
+        Horario = "5x por semana",
+        Capacidade = 6,
+        Treinador = "Treinador D",
+        TipoDeAula = "Alta Intensidade"
+    },
+    new PlanoTreino
+    {
+        Id = 5,
+        Nome = "Plano Supernova",
+        Horario = "6x por semana",
+        Capacidade = 4,
+        Treinador = "Treinador Elite",
+        TipoDeAula = "Performance Máxima"
+    }
+};
 
         public IActionResult Index()
         {
--- UC000608/Models/TipoPlano.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UC000608.Models
-{
-    public class TipoPlano
-    {
-        public int Id { get; set; }
-        public string? Nome { get; set; }
-        public int Desconto { get; set; } 
-    }
-}
