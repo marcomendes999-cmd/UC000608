d04b5952a9830db6cc2f96c7a0f7f15649aeea3a
MuscledDog <lemos.vasco@gmail.com>
2026-04-16 21:25:14 +0100

    Tipo Plano_novo

---
+++ UC000608/Models/TipoPlano.cs
@@ -0,0 +1,9 @@
+namespace UC000608.Models
+{
+    public class TipoPlano
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Desconto { get; set; }
+    }
+}
