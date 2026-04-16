29adcf39e5e2ca4edb8885c531cbd03edb11ba18
Maria-Helena-Almeida <mariahelena.nena@hotmail.com>
2026-04-16 22:07:26 +0100

    add class Precença com os atributos: ID; Aula; Lista de Membros;

---
+++ UC000608/Models/Presenca.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC000608.Models
+
+{
+    public class Presenca
+    {
+        public int Id { get; private set; }
+
+        // Relação com Aula
+        public int AulaId { get; set; }
+        public Aula Aula { get; set; }
+
+        // Lista de membros presentes
+        public List<Membro> Membros { get; private set; }
+
+        // Construtor
+        public Presenca(int id)
+        {
+            Id = id;
+            Membros = new List<Membro>();
+        }
+
+        // Métodos (comportamento)
+        public void AdicionarMembro(Membro membro)
+        {
+            if (!Membros.Contains(membro))
+            {
+                Membros.Add(membro);
+            }
+        }
+
+        public void RemoverMembro(Membro membro)
+        {
+            if (Membros.Contains(membro))
+            {
+                Membros.Remove(membro);
+            }
+        }
+    }
+}
