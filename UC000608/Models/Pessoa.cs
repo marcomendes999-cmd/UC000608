f6e6238a36c57b8f028ddb76b2f1718201390492
FilipaBarreto <replayandreplay@gmail.com>
2026-04-23 22:34:04 +0100

    Acrescentar mais dados para validar campos

--- UC000608/Models/Pessoa.cs
+++ UC000608/Models/Pessoa.cs
@@ -1,10 +1,19 @@
-﻿namespace UC000608.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UC000608.Models
 {
     public class Pessoa
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; } ="";
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        public string Email { get; set; } = "";
+
+        public string Telefone { get; set; } = "";
+
+        public int Idade { get; set; } 
     }
 }
